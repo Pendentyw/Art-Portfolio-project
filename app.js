3ef0d3b9cf85da65ed53e21c87b13a35a6e6c955
Pendentyw <pal7fal7@gmail.com>
2023-09-21 21:20:19 +0200

    new project with sketch and some styles

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('XD');
+if (module.hot) {
+  module.hot.accept();
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('nav').classList.remove('nav-animation');
+  } else {
+    document.querySelector('nav').classList.add('nav-animation');
+  }
+  prevScrollpos = currentScrollPos;
+};
