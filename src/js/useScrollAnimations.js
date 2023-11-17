0c08249e140fd6dceae81d9e0bcc0430b27b7535
Pendentyw <pal7fal7@gmail.com>
2023-11-17 18:21:31 +0100

    some changes in animaions and queries

--- src/js/useScrollAnimations.js
+++ src/js/useScrollAnimations.js
@@ -1,24 +1,46 @@
 import { addScrollAnimation } from './components/scrollAnimation';
 
 export const useScrollAnimations = () => {
-  const rotateScrollAnimation = document.querySelectorAll('.rotate-animation');
-  const opacityScrollAnimation = document.querySelectorAll('.opacity-animation');
-  const easeInLeftScrollAnimation = document.querySelectorAll('.ease-in-left-animation');
-  const fadeUpAnimation = document.querySelectorAll('.fade-up-animation');
-  const borderRadiusAnimation = document.querySelectorAll('.border-radius-animation');
-  const zoomAnimation = document.querySelectorAll('.zoom-animation');
-  const gifAndBorderAnimation = document.querySelectorAll('.border-right-animation');
-  const widthAnimation = document.querySelectorAll('.width-animation');
-  const easeInRightScrollAnimation = document.querySelectorAll('.ease-in-right-animation');
+  [
+    ['rotate', true],
+    ['opacity', true],
+    ['ease-in-left'],
+    ['fade-up'],
+    ['fade-down'],
+    ['border-radius', true],
+    ['zoom', true],
+    ['border-right', true],
+    ['width', true],
+    ['ease-in-right'],
+  ].forEach((tagPair) => {
+    const [tag, flag] = tagPair;
 
-  //use animation on scroll function
-  addScrollAnimation(borderRadiusAnimation, 'border-radius-scroll-animation', true);
-  addScrollAnimation(rotateScrollAnimation, 'rotate-scroll-animation', true);
-  addScrollAnimation(opacityScrollAnimation, 'opacity-scroll-animation', true);
-  addScrollAnimation(easeInLeftScrollAnimation, 'ease-in-left-scroll-animation');
-  addScrollAnimation(easeInRightScrollAnimation, 'ease-in-right-scroll-animation');
-  addScrollAnimation(fadeUpAnimation, 'fade-up-scroll-animation', false);
-  addScrollAnimation(zoomAnimation, 'zoom-scroll-animation', true);
-  addScrollAnimation(gifAndBorderAnimation, 'border-right-scroll-animation', true);
-  addScrollAnimation(widthAnimation, 'width-scroll-animation', true);
+    addScrollAnimation(
+      document.querySelectorAll(`.${tag}-animation`),
+      `${tag}-animation-scroll`,
+      flag
+    );
+  });
+
+  //   const rotateScrollAnimation = document.querySelectorAll('.rotate-animation');
+  //   const opacityScrollAnimation = document.querySelectorAll('.opacity-animation');
+  //   const easeInLeftScrollAnimation = document.querySelectorAll('.ease-in-left-animation');
+  //   const fadeUpAnimation = document.querySelectorAll('.fade-up-animation');
+  //   const borderRadiusAnimation = document.querySelectorAll('.border-radius-animation');
+  //   const zoomAnimation = document.querySelectorAll('.zoom-animation');
+  //   const gifAndBorderAnimation = document.querySelectorAll('.border-right-animation');
+  //   const widthAnimation = document.querySelectorAll('.width-animation');
+  //   const easeInRightScrollAnimation = document.querySelectorAll('.ease-in-right-animation');
+  //   const fadeDownAnimation = document.querySelectorAll();
+
+  //   //use animation on scroll function
+  //   addScrollAnimation(borderRadiusAnimation, 'border-radius-animation-scroll', true);
+  //   addScrollAnimation(rotateScrollAnimation, 'rotate-scroll-animation', true);
+  //   addScrollAnimation(opacityScrollAnimation, 'opacity-scroll-animation', true);
+  //   addScrollAnimation(easeInLeftScrollAnimation, 'ease-in-left-scroll-animation');
+  //   addScrollAnimation(easeInRightScrollAnimation, 'ease-in-right-scroll-animation');
+  //   addScrollAnimation(fadeUpAnimation, 'fade-up-scroll-animation', false);
+  //   addScrollAnimation(zoomAnimation, 'zoom-scroll-animation', true);
+  //   addScrollAnimation(gifAndBorderAnimation, 'border-right-scroll-animation', true);
+  //   addScrollAnimation(widthAnimation, 'width-scroll-animation', true);
 };
