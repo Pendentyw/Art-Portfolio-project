9922b8b699a6b9bbbfb5eb0205403ce454eef4eb
Pendentyw <pal7fal7@gmail.com>
2024-01-29 16:16:55 +0100

    text changed

--- src/js/components/navbarAnimation.js
+++ src/js/components/navbarAnimation.js
@@ -5,8 +5,6 @@ export const navbarAnimation = () => {
   window.onscroll = function () {
     const currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
-      // document.querySelector('nav').classList.remove('nav-animation');
-      // document.querySelector('nav').classList.add('nav-animation-reverse');
       navbar.classList.replace('nav-animation', 'nav-animation-reverse');
     } else {
       document.querySelector('nav').classList.remove('nav-animation-reverse');
