1aba908705ac9ff7f3fd91437396800f1d1c6a92
Pendentyw <pal7fal7@gmail.com>
2024-01-31 16:18:22 +0100

    url changes test

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-import slider from './src/js/components/carousel.js';
-import { addScrollAnimation } from './src/js/components/scrollAnimation.js';
 import { useScrollAnimations } from './src/js/useScrollAnimations.js';
-import { carousel } from './src/js/components/carousel.js';
-import { playVideoOnScroll, pauseVideoOnMouseEnter } from './src/js/components/playVideo.js';
-import { lazyLoad } from './src/js/components/lazyLoad.js';
-import { previewGalleryImage } from '/src/js/components/previewGalleryImage.js';
 import { dropdown } from './src/js/components/dropdown.js';
 import { navbarAnimation } from './src/js/components/navbarAnimation.js';
 
---
+++ src/js/photographyPage.js
@@ -0,0 +1,3 @@
+import { simplePages } from './simplePages.js';
+
+simplePages();
